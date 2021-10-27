fb2241579baf89cd7627cb083ced63984ec9ea4c
Brian Anderson <andersrb@gmail.com>
2021-10-27 13:09:16 -0600

    .

--- src/slq/src/lib.rs
+++ src/slq/src/lib.rs
@@ -7,6 +7,9 @@ use solana_program::{
     entrypoint::ProgramResult,
     program_error::ProgramError,
     pubkey::Pubkey,
+    system_program,
+    system_instruction,
+    program::invoke_signed,
     msg,
 };
 use std::convert::{TryFrom, TryInto};
@@ -28,12 +31,14 @@ fn process_instruction(
         Instruction::CreateVault(instr) => {
             let payer = next_account_info(accounts_iter)?;
             let vault = next_account_info(accounts_iter)?;
-            instr.exec(program_id, payer, vault)?;
+            let system_program = next_account_info(accounts_iter)?;
+            instr.exec(program_id, payer, vault, system_program)?;
         }
         Instruction::DepositToVault(instr) => {
             let payer = next_account_info(accounts_iter)?;
             let vault = next_account_info(accounts_iter)?;
-            instr.exec(program_id, payer, vault)?;
+            let system_program = next_account_info(accounts_iter)?;
+            instr.exec(program_id, payer, vault, system_program)?;
         }
     }
 
@@ -51,6 +56,7 @@ pub enum Instruction {
 ///
 /// - 0: payer: signer, writable
 /// - 1: vault: pda, writable, owner=program?
+/// - 2: system_program: executable
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct CreateVault {
     pub vault_bump_seed: u8,
@@ -60,6 +66,7 @@ pub struct CreateVault {
 ///
 /// - 0: payer: signer, writable
 /// - 1: vault: pda, writable, owner=program
+/// - 2: system_program: executable
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct DepositToVault {
     pub vault_bump_seed: u8,
@@ -67,21 +74,55 @@ pub struct DepositToVault {
 }
 
 impl CreateVault {
-    fn exec(&self, program_id: &Pubkey, payer: &AccountInfo, vault: &AccountInfo) -> ProgramResult {
+    fn exec<'accounts>(&self,
+                program_id: &Pubkey,
+                payer: &AccountInfo<'accounts>,
+                vault: &AccountInfo<'accounts>,
+                system_program: &AccountInfo) -> ProgramResult
+    {
         assert!(payer.is_signer);
         assert!(payer.is_writable);
         // todo vault asserts
+        assert!(system_program.executable);
 
         let (vault_, vault_bump_seed_) = vault_pda(program_id, payer.key);
         assert_eq!(vault.key, &vault_);
         assert_eq!(self.vault_bump_seed, vault_bump_seed_);
 
-        todo!()
+        let lamports = 1000; // todo
+
+        invoke_signed(
+            &system_instruction::create_account(
+                payer.key,
+                vault.key,
+                lamports,
+                0,
+                program_id,
+            ),
+            &[
+                payer.clone(),
+                vault.clone(),
+            ],
+            &[
+                &[
+                    b"vault",
+                    payer.key.as_ref(),
+                    &[self.vault_bump_seed],
+                ],
+            ]
+        )?;
+
+        Ok(())
     }
 }
 
 impl DepositToVault {
-    fn exec(&self, program_id: &Pubkey, payer: &AccountInfo, vault: &AccountInfo) -> ProgramResult {
+    fn exec(&self,
+            program_id: &Pubkey,
+            payer: &AccountInfo,
+            vault: &AccountInfo,
+            system_program: &AccountInfo) -> ProgramResult
+    {
         todo!()
     }
 }
